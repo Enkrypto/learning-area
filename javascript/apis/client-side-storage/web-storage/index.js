638d1732ee5cc732eebcb6572ac33d6e5f22129c
Enkrypto <alec19097@live.ytech.edu>
2018-12-17 09:11:54 -0500

    Finished Web Storage example

--- javascript/apis/client-side-storage/web-storage/index.js
+++ javascript/apis/client-side-storage/web-storage/index.js
@@ -1,19 +1,18 @@
 // create needed constants
 const rememberDiv = document.querySelector('.remember');
 const forgetDiv = document.querySelector('.forget');
-const form = document.querySelector('.form');
+const form = document.querySelector('form');
 const nameInput = document.querySelector('#entername');
 const submitBtn = document.querySelector('#submitname');
 const forgetBtn = document.querySelector('#forgetname');
 
 const h1 = document.querySelector('h1');
-cosnt personalGreeting = document.querySelector('.personal-greeting');
+const personalGreeting = document.querySelector('.personal-greeting');
 
 // Stop the form from submitting when a button is pressed
-form.addEventHandler('submit', e => {
+form.addEventListener('submit', e => {
   e.preventDefault();
-});
-
+})
 // run the function when the 'Say hello' button is clicked
 submitBtn.addEventListener('click', () => {
   // store the entered name in web storage
@@ -22,3 +21,35 @@ submitBtn.addEventListener('click', () => {
   // and updating the form display
   nameDisplayCheck();
 });
+
+// run the function when the 'Forget' button is clicked
+forgetBtn.addEventListener('click', () => {
+  // Remove the stored name from web storage
+  localStorage.removeItem('name');
+  // run nameDisplayCheck() to sort out displaying the generic greeting again
+  // and updating the form display
+  nameDisplayCheck();
+});
+
+// define the nameDisplayCheck() function
+function nameDisplayCheck() {
+  // check whether the 'name' data item is stored in web storage
+  if(localStorage.getItem('name')) {
+    // If it is, display personalized greeting
+    let name = localStorage.getItem('name');
+    h1.textContent = 'Welcome, ' + name;
+    personalGreeting.textContent = 'Welcome to our website, ' + name + '! We hope you have fun while you are here.';
+    // hide the 'remember' part of the form and show the 'forget' part
+    forgetDiv.style.display = 'block';
+    rememberDiv.style.display = 'none';
+  } else {
+    // if not, display generic greeting
+    h1.textContent = 'Welcome to our website.';
+    personalGreeting.textContent = 'Welcome to our website. We hope you have fun while you are here.';
+    // hide the 'forget' part of the form and show the 'remember' part
+    forgetDiv.style.display = 'none';
+    rememberDiv.style.display = 'block';
+  }
+}
+
+document.body.addEventListener('load', nameDisplayCheck);
