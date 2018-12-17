af980d883a5ee3a15cbda6e3be6faa5b7acb6600
Enkrypto <alec19097@live.ytech.edu>
2018-12-16 21:49:47 -0500

    initial commit

---
+++ javascript/apis/client-side-storage/web-storage/index.js
@@ -0,0 +1,24 @@
+// create needed constants
+const rememberDiv = document.querySelector('.remember');
+const forgetDiv = document.querySelector('.forget');
+const form = document.querySelector('.form');
+const nameInput = document.querySelector('#entername');
+const submitBtn = document.querySelector('#submitname');
+const forgetBtn = document.querySelector('#forgetname');
+
+const h1 = document.querySelector('h1');
+cosnt personalGreeting = document.querySelector('.personal-greeting');
+
+// Stop the form from submitting when a button is pressed
+form.addEventHandler('submit', e => {
+  e.preventDefault();
+});
+
+// run the function when the 'Say hello' button is clicked
+submitBtn.addEventListener('click', () => {
+  // store the entered name in web storage
+  localStorage.setItem('name', nameInput.value);
+  // run nameDisplayCheck() to sort out displaying the personalized greetings
+  // and updating the form display
+  nameDisplayCheck();
+});
