15f9c30ff3403129fe4c39ea1edb35c2a43b406a
Enkrypto <alec19097@live.ytech.edu>
2018-12-18 16:04:10 -0500

    Completed index.js, database should function as expected.

--- javascript/apis/client-side-storage/indexeddb/notes/index.js
+++ javascript/apis/client-side-storage/indexeddb/notes/index.js
@@ -8,35 +8,35 @@ const submitBtn = document.querySelector('form button');
 // Create an instance of a db object for us to store the open database in
 let db;
 
-window.onload = function() {
+window.addEventListener('load', () => {
   // Open our database; it is created if it doesn't already exist
-  // (see onupgradeneeded below)
+  // (see unupgradeneeded below)
   let request = window.indexedDB.open('notes', 1);
 
-  // onerror handler signifies that the database didn't open successfully
-  request.onerror = function() {
+  // onerror handler signified that the database didn't open successfully
+  request.addEventListener('error', () => {
     console.log('Database failed to open');
-  };
+  });
 
   // onsuccess handler signifies that the database opened successfully
-  request.onsuccess = function() {
-    console.log('Database opened succesfully');
+  request.addEventListener('success', () => {
+    console.log('Database opened successfully');
 
-    // Store the opened database object in the db variable. This is used a lot below
+    // Store the opened database object in the db variable
+    // This is used a lot below
     db = request.result;
 
     // Run the displayData() function to display the notes already in the IDB
     displayData();
-  };
+  });
 
   // Setup the database tables if this has not already been done
-  request.onupgradeneeded = function(e) {
-
+  request.addEventListener('upgradeneeded', e => {
     // Grab a reference to the opened database
     let db = e.target.result;
 
     // Create an objectStore to store our notes in (basically like a single table)
-    // including a auto-incrementing key
+    // including an auto-incrementing key
     let objectStore = db.createObjectStore('notes', { keyPath: 'id', autoIncrement:true });
 
     // Define what data items the objectStore will contain
@@ -44,10 +44,10 @@ window.onload = function() {
     objectStore.createIndex('body', 'body', { unique: false });
 
     console.log('Database setup complete');
-  };
+  });
 
   // Create an onsubmit handler so that when the form is submitted the addData() function is run
-  form.onsubmit = addData;
+  form.addEventListener('submit', addData);
 
   // Define the addData() function
   function addData(e) {
@@ -64,30 +64,30 @@ window.onload = function() {
     let objectStore = transaction.objectStore('notes');
 
     // Make a request to add our newItem object to the object store
-    var request = objectStore.add(newItem);
-    request.onsuccess = function() {
+    let request = objectStore.add(newItem);
+    request.addEventListener('success', () => {
       // Clear the form, ready for adding the next entry
       titleInput.value = '';
       bodyInput.value = '';
-    };
+    });
 
     // Report on the success of the transaction completing, when everything is done
-    transaction.oncomplete = function() {
+    transaction.addEventListener('complete', () => {
       console.log('Transaction completed: database modification finished.');
 
       // update the display of data to show the newly added item, by running displayData() again.
       displayData();
-    };
+    });
 
-    transaction.onerror = function() {
+    transaction.addEventListener('error', () => {
       console.log('Transaction not opened due to error');
-    };
+    });
   }
 
   // Define the displayData() function
   function displayData() {
     // Here we empty the contents of the list element each time the display is updated
-    // If you ddn't do this, you'd get duplicates listed each time a new note is added
+    // If you didn't do this, you'd get duplicates listed each time a new note is added
     while (list.firstChild) {
       list.removeChild(list.firstChild);
     }
@@ -95,13 +95,13 @@ window.onload = function() {
     // Open our object store and then get a cursor - which iterates through all the
     // different data items in the store
     let objectStore = db.transaction('notes').objectStore('notes');
-    objectStore.openCursor().onsuccess = function(e) {
+    objectStore.openCursor().addEventListener('success', e => {
       // Get a reference to the cursor
       let cursor = e.target.result;
 
       // If there is still another data item to iterate through, keep running this code
       if(cursor) {
-        // Create a list item, h3, and p to put each data item inside when displaying it
+        // create a list item, h3, and p to put each data item inside when display it
         // structure the HTML fragment, and append it inside the list
         let listItem = document.createElement('li');
         let h3 = document.createElement('h3');
@@ -126,49 +126,48 @@ window.onload = function() {
 
         // Set an event handler so that when the button is clicked, the deleteItem()
         // function is run
-        deleteBtn.onclick = deleteItem;
+        deleteBtn.addEventListener('click', deleteItem);
 
         // Iterate to the next item in the cursor
         cursor.continue();
       } else {
-        // Again, if list item is empty, display a 'No notes stored' message
+        // Again, if the list item is empty, display a 'No notes stored' message
         if(!list.firstChild) {
           let listItem = document.createElement('li');
-          listItem.textContent = 'No notes stored.'
+          listItem.textContent = 'No notes stored.';
           list.appendChild(listItem);
         }
         // if there are no more cursor items to iterate through, say so
         console.log('Notes all displayed');
       }
-    };
+    });
+
+    // Define the deleteItem() function
+    function deleteItem(e) {
+      // retrieve the name of the task we want to delete. We need
+      // to convert it to a number before trying to use it with IDB; IDB key
+      // values are type-sensitive.
+      let noteId = Number(e.target.parentNode.getAttribute('data-note-id'));
+
+      // open a database transaction and delete the task, finding it using the id we retrieved above
+      let transaction = db.transaction(['notes'], 'readwrite');
+      let objectStore = transaction.objectStore('notes');
+      let request = objectStore.delete(noteId);
+
+      // report that the data item has been deleted
+      transaction.addEventListener('complete', () => {
+        // delete the parent of the button
+        // which is the list item, so it is no longer displayed
+        e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+        console.log('Note ' + noteId + ' deleted.');
+
+        // Again, if the list item is empty, display a 'No notes stored' message
+        if(!list.firstChild) {
+          let listItem = document.createElement('li');
+          listItem.textContent = 'No notes stored.';
+          list.appendChild(listItem);
+        }
+      });
+    }
   }
-
-  // Define the deleteItem() function
-  function deleteItem(e) {
-    // retrieve the name of the task we want to delete. We need
-    // to convert it to a number before trying it use it with IDB; IDB key
-    // values are type-sensitive.
-    let noteId = Number(e.target.parentNode.getAttribute('data-note-id'));
-
-    // open a database transaction and delete the task, finding it using the id we retrieved above
-    let transaction = db.transaction(['notes'], 'readwrite');
-    let objectStore = transaction.objectStore('notes');
-    let request = objectStore.delete(noteId);
-
-    // report that the data item has been deleted
-    transaction.oncomplete = function() {
-      // delete the parent of the button
-      // which is the list item, so it is no longer displayed
-      e.target.parentNode.parentNode.removeChild(e.target.parentNode);
-      console.log('Note ' + noteId + ' deleted.');
-
-      // Again, if list item is empty, display a 'No notes stored' message
-      if(!list.firstChild) {
-        let listItem = document.createElement('li');
-        listItem.textContent = 'No notes stored.';
-        list.appendChild(listItem);
-      }
-    };
-  }
-
-};
+});
